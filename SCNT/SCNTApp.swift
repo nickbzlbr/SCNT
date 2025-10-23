6317371c80c4c016a21471122c7377bc23874e07
Nick Betzlbacher <nbetzlbacher.hcie-m2019@fh-salzburg.ac.at>
2025-10-23 13:56:40 +0200

    Initial Commit

---
+++ SCNT/SCNTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SCNTApp.swift
+//  SCNT
+//
+//  Created by Nick Betzlbacher on 23.10.25.
+//
+
+import SwiftUI
+
+@main
+struct SCNTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
